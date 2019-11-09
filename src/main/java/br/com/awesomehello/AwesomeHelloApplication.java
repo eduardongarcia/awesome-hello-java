556d63acd8d193c02bd811e0e0a0b9429461047a
eduardongarcia <dudueco@hotmail.com>
2019-11-09 08:21:48 -0300

    Add spring boot starters

---
+++ src/main/java/br/com/awesomehello/AwesomeHelloApplication.java
@@ -0,0 +1,13 @@
+package br.com.awesomehello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomeHelloApplication.class, args);
+	}
+
+}
