75f1f16aa628793af5bee7c726c3083891d087a5
eduardongarcia <dudueco@hotmail.com>
2019-11-11 12:45:59 -0300

    Rename integration test with ITSpec sufix

--- src/test/groovy/br/com/awesomehello/entrypoint/rest/ExampleControllerITSpec.groovy
+++ src/test/groovy/br/com/awesomehello/entrypoint/rest/ExampleControllerITSpec.groovy
@@ -6,14 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-
 import spock.lang.Specification
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @WebMvcTest(ExampleController.class)
-class ExampleControllerSpec extends Specification {
+class ExampleControllerITSpec extends Specification {
     @Autowired
     MockMvc mvc
 
